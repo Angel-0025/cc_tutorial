91c5cae7dac3278eb5aefffdaa25502bab14d271
Angel Oribiada <oribiada24@gmail.com>
2023-08-25 18:05:52 +0800

    finishing  header 08-25-23

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -70,11 +70,10 @@ function Employees() {
   const showEmployee = true;
 
   return (
-    <div className="App bg-gray-300 min-h-screen">
-
+    <div className="">
       {showEmployee ? (
         <>
-          <div className="flex flex-wrap justify-center my-2">
+          <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
               // Different way to pass the data // props
               const editEmployee = (
