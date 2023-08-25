3190bb01ad7586f1e8af194811e28eb5a3babd03
Angel Oribiada <oribiada24@gmail.com>
2023-08-25 17:49:38 +0800

    active page link 08-25-23

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./index.css";
 import Employees from "./pages/Employees";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Customer from "./pages/Customer";
 
 function App() {
   return (
-    <Header>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Header>
         <Routes>
           <Route path="/Employees" element={<Employees />} />
+          <Route path="/Customer" element={<Customer />} />
         </Routes>
-      </BrowserRouter>
-    </Header>
+      </Header>
+    </BrowserRouter>
   );
 }
 
