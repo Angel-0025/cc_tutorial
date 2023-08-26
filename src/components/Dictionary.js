a27606a2283367460ef35f6eff856778fca28c61
Angel Oribiada <oribiada24@gmail.com>
2023-08-26 18:48:25 +0800

    useEffect deoendecy  Array 08-26-23

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from "react";
 
 const Dictionary = () => {
   const [word, setWord] = useState();
+  const [word2, setWord2] = useState();
 
   useEffect(() => {
     console.log("State updated", word);
-  });
+  }, [word]);
+
+  useEffect(() => {
+    console.log("State updated", word2);
+  }, [word2]);
 
   return (
     <>
@@ -16,6 +21,14 @@ const Dictionary = () => {
         }}
       />
       <h1>Let's get the definition for {word}</h1>
+
+      <input
+        type="text"
+        onChange={(e) => {
+          setWord2(e.target.value);
+        }}
+      />
+      <h2>Let's get the definition for {word2}</h2>
     </>
   );
 };
