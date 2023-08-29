fe89d168e78d0e4bc8d54efb55df7da76879aa78
Angel Oribiada <oribiada24@gmail.com>
2023-08-29 18:46:37 +0800

    create 404 page 08-29-23

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,32 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import NotFound from "../components/NotFound";
 
 const Definition = () => {
   const [word, setWord] = useState([]);
+  const [notFound, setNotFount] = useState(false);
   let { search } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === 404) {
+          setNotFount(true);
+        }
+        return response.json();
+      })
       .then((data) => {
         setWord(data[0].meanings);
         console.log(data[0].meanings);
       });
   }, []);
 
+  if (notFound === true) {
+    return (
+      <>
+        <NotFound />
+        <Link to="/dictionary">Search Another</Link>
+      </>
+    );
+  }
   return (
     <>
-      <h1>Here is a definition: </h1>
-      {word
-        ? word.map((meaning) => {
+      {word ? (
+        <>
+          <h1>Here is a definition: </h1>
+          {word.map((meaning) => {
             return (
               <p key={uuidv4()}>
                 {meaning.partOfSpeech} : {meaning.definitions[0].definition}
               </p>
             );
-          })
-        : null}
+          })}
+        </>
+      ) : null}
     </>
   );
 };
